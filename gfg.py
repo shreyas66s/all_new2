b0bf1639ada801f3a3a8b9a6c2cc670dffbf514a
shreyas66s <shreyas1665@gmail.com>
2023-11-02 16:28:57 +0530

    First commit

---
+++ gfg.py
@@ -0,0 +1,9 @@
+"""A simple script that demonstrates addition in Python."""
+
+# Define constants A and B
+A = 3
+B = 2
+
+# Calculate the sum of A and B and print the result
+RESULT = A + B
+print(RESULT)
